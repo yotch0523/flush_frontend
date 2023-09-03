4fda39fb523f69ff8ad4c864af795128b6e13dbe
Youki YOSHIOKA <yotch.youki.you@gmail.com>
2023-09-03 21:43:43 +0900

    implemented course component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
 import styled from 'styled-components'
 import CardContainer from '~/components/common/CardContainer'
+import CourseContainer from '~/components/common/CourseContainer'
 import HomeLayout from '~/layouts/HomeLayout'
 
 const Home = () => {
   return (
     <HomeLayout>
-      <section>
+      <StyledSection>
         <h1>カード</h1>
         <CardContainer cards={[]}></CardContainer>
-      </section>
+      </StyledSection>
+      <StyledSection>
+        <h1>コース</h1>
+        <CourseContainer courses={[]}></CourseContainer>
+      </StyledSection>
     </HomeLayout>
   )
 }
+
+// style
+const StyledSection = styled.section`
+  height: 160px;
+  width: 100%;
+`
 
 export default Home
